52ceb08c188a08fa661fd0f498d5c969cba1d379
college_demo <kamrunhasan12@gmail.com>
2024-02-08 05:18:15 +0600

    notification update

--- lib/Screens/learning.dart
+++ lib/Screens/learning.dart
@@ -8,6 +8,7 @@ import 'package:realpalooza/components/my_text_field.dart';
 import 'package:realpalooza/pages/comment.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../Theme/theme_provider.dart';
+import '../pages/My_learning.dart';
 import 'chatPage.dart';
 
 class LearnProgrammingPage extends StatelessWidget {
@@ -130,6 +131,321 @@ class LearnProgrammingPage extends StatelessWidget {
               // Add more URLs for the topic as needed
             ],
           ),
+          TopicButton(
+            topicName: 'Sum of Digits',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Prefix sum',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Reverse an array',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: '2D Prefix Sum',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Java',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Kadane Algorithm',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Prime Number',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Sieve of Eratosthenes',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Segmented Sieve',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Linear Diophantine Equations',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Modular Operations',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Modular Inverse',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Matrix Exponentiation',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Find all divisors of a natural number',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Bitset CPP',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Euler’s Totient Function',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Euler’s Totient function for all numbers smaller than or equal to n',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Inclusion Exclusion Principle ',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Pigeon Hole Principle',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Power(x, y) in O( logN )',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'nCr % mod for multiple queries',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Gaussian Elimination',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Catalan numbers: Applications',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'nCr % mod for multiple queries',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Linear Search Algorithm',
+            urls: [
+              "https://www.geeksforgeeks.org/linear-search/",
+              "https://www.tutorialspoint.com/data_structures_algorithms/linear_search_algorithm.htm",
+              "https://www.javatpoint.com/linear-search",
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Binary Search',
+            urls: [
+              "https://www.geeksforgeeks.org/binary-search/",
+              "https://www.programiz.com/dsa/binary-search",
+              "https://www.youtube.com/watch?app=desktop&v=ARtYEZjOTbU&ab_channel=DBSTalks"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Unbounded Binary Search',
+            urls: [
+              "https://www.geeksforgeeks.org/find-the-point-where-a-function-becomes-negative/",
+              "https://www.techiedelight.com/unbounded-binary-search/",
+              "https://stackoverflow.com/questions/66609233/unbounded-binary-search-finding-the-point-where-a-monotonically-increasing-func"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Merge Sort',
+            urls: [
+              "https://www.geeksforgeeks.org/merge-sort/",
+              "https://www.programiz.com/dsa/merge-sort",
+              "https://www.javatpoint.com/merge-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'QuickSort',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Sum of Digits',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Prefix sum',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Reverse an array',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Find all divisors of a natural number',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Bitset CPP',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
+          TopicButton(
+            topicName: 'Euler’s Totient Function',
+            urls: [
+              "https://geeksforgeeks.org/quick-sort/",
+              "https://www.programiz.com/dsa/quick-sort",
+              "https://www.javatpoint.com/quick-sort"
+              // Add more URLs for the topic as needed
+            ],
+          ),
 
           // Add more TopicButtons as needed
         ],
@@ -139,214 +455,3 @@ class LearnProgrammingPage extends StatelessWidget {
   }
 }
 
-class TopicButton extends StatelessWidget {
-  final String topicName;
-  final List<String> urls;
-
-  TopicButton({required this.topicName, required this.urls});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListTile(
-        title: Text(
-          topicName,
-          style: TextStyle(
-            fontFamily: 'Comfortaa',
-            fontSize: 17,
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-        ),
-        trailing: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return TopicDetailPage(topicName: topicName, urls: urls);
-                },
-              ),
-            );
-          },
-          child: Text(
-            'Explore',
-            style: TextStyle(color: Color(0xff26b051)),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TopicDetailPage extends StatelessWidget {
-  final String topicName;
-  final List<String> urls;
-  
-  final currentUser=FirebaseAuth.instance.currentUser!;
-  final textController=TextEditingController();
-
-  TopicDetailPage({required this.topicName, required this.urls});
-
-  //post message
-
-  void postMessage(){
-    //only post if there is something in the textfield
-    if(textController.text.isNotEmpty){
-      //store in firebase
-      FirebaseFirestore.instance.collection("User posts").add({
-        'UserEmail':currentUser.email,
-        'Message':textController.text,
-        'TimeStamp':Timestamp.now(),
-      });
-    }
-
-  }
-
-  //add a comment
-  void addComment(String commentText){
-    //write the comment to firestore under the comments collection for this page
-    var widget;
-    var currentUser;
-    FirebaseFirestore.instance.
-    collection("User Posts").
-    doc(widget.postId).
-    collection("Comment").
-    add({
-      "CommentText":commentText,
-      "CommentedBy":currentUser.email,
-      "CommentTime":Timestamp.now()
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        centerTitle: true,
-        title: Text(topicName,style: TextStyle(
-          fontFamily: 'Comfortaa',
-          fontSize: 20,
-          color: Theme.of(context).colorScheme.secondary,
-        ),
-        ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-              },
-              icon: Icon(
-                Theme.of(context).brightness == Brightness.dark
-                    ? Icons.dark_mode
-                    : Icons.dark_mode_outlined,
-                color: Theme.of(context).brightness == Brightness.dark
-                    ?Colors.white.withOpacity(.8)
-                    :Colors.grey[800],
-              ),
-            ),
-          ],
-
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(25),
-              bottomRight: Radius.circular(25),
-            ),
-          )
-      ),
-      body: Expanded(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: urls.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      urls[index],
-                      style: TextStyle(
-                        fontFamily: 'Comfortaa',
-                        fontSize: 18,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                    onTap: () {
-                      launchURL(urls[index]);
-                    },
-                  );
-                },
-              ),
-            ),
-            
-            Expanded(
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection("User Posts")
-                      .orderBy(
-                         "TimeStamp",
-                        descending: false,
-                  ).snapshots(),
-                  builder: (context,snapshot){
-                    if(snapshot.hasData){
-                      return ListView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (context,index){
-                            //get the message
-                            final post=snapshot.data!.docs[index];
-                            return Comment(
-                                message: post['Message'],
-                                user: post['UserEmail'],
-                            );
-                          },
-                      );
-                    }else if(snapshot.hasError){
-                      return Center(
-                        child: Text('Error:${snapshot.error}'),
-                      );
-                    }
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                ),
-            ),
-
-            Padding(padding: const EdgeInsets.all(25.0),
-            child: Row(
-              children: [
-                Expanded(
-                    child: Mytextfield(
-                      controller: textController,
-                      hintText: 'Write your comment..',
-                       obscuretext: false ,
-                    ),
-                ),
-                Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      shape: BoxShape.circle,
-                    ),
-                    //margin: const EdgeInsets.only(right: 25),
-                    child: IconButton(onPressed: postMessage, icon: Icon(Icons.send),
-                    ))
-              ],
-            ),
-            )
-          ],
-        ),
-      )
-    );
-  }
-
-  void launchURL(String url) async {
-    // ignore: deprecated_member_use
-    if (await canLaunch(url)) {
-      // ignore: deprecated_member_use
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
